89c60ce86bb8d93bc91b010b19b08e1813a42b9b
cketti <cketti@gmail.com>
2020-09-30 02:37:24 +0200

    Improve preview text extraction
    Using 'EmailSectionExtractor' provides the necessary context to reliably detect quote headers, i.e. text like "On $date $sender wrote:". The regular expression previously used would remove almost any line ending with ":".

--- app/core/src/main/java/com/fsck/k9/message/extractors/PreviewTextExtractor.kt
+++ app/core/src/main/java/com/fsck/k9/message/extractors/PreviewTextExtractor.kt
@@ -3,6 +3,8 @@ package com.fsck.k9.message.extractors
 import com.fsck.k9.mail.Part
 import com.fsck.k9.mail.internet.MessageExtractor
 import com.fsck.k9.mail.internet.MimeUtility.isSameMimeType
+import com.fsck.k9.message.html.EmailSection
+import com.fsck.k9.message.html.EmailSectionExtractor
 import com.fsck.k9.message.html.HtmlConverter.htmlToText
 
 internal class PreviewTextExtractor {
@@ -24,38 +26,78 @@ internal class PreviewTextExtractor {
     }
 
     private fun stripTextForPreview(text: String): String {
-        // Remove (correctly delimited by '-- \n') signatures
-        var text = text.replace("(?ms)^-- [\\r\\n]+.*".toRegex(), "")
+        var intermediateText = text
+
+        intermediateText = normalizeLineBreaks(intermediateText)
+        intermediateText = stripSignature(intermediateText)
+        intermediateText = extractUnquotedText(intermediateText)
+
         // try to remove lines of dashes in the preview
-        text = text.replace("(?m)^----.*?$".toRegex(), "")
-        // remove quoted text from the preview
-        text = text.replace("(?m)^[#>].*$".toRegex(), "")
-        // Remove a common quote header from the preview
-        text = text.replace("(?m)^On .*wrote.?$".toRegex(), "")
-        // Remove a more generic quote header from the preview
-        text = text.replace("(?m)^.*\\w+:$".toRegex(), "")
+        intermediateText = intermediateText.replace("(?m)^----.*?$".toRegex(), "")
         // Remove horizontal rules.
-        text = text.replace("\\s*([-=_]{30,}+)\\s*".toRegex(), " ")
+        intermediateText = intermediateText.replace("\\s*([-=_]{30,}+)\\s*".toRegex(), " ")
 
         // URLs in the preview should just be shown as "..." - They're not
         // clickable and they usually overwhelm the preview
-        text = text.replace("https?://\\S+".toRegex(), "...")
+        intermediateText = intermediateText.replace("https?://\\S+".toRegex(), "...")
         // Don't show newlines in the preview
-        text = text.replace("(\\r|\\n)+".toRegex(), " ")
+        intermediateText = intermediateText.replace("[\\r\\n]+".toRegex(), " ")
         // Collapse whitespace in the preview
-        text = text.replace("\\s+".toRegex(), " ")
+        intermediateText = intermediateText.replace("\\s+".toRegex(), " ")
         // Remove any whitespace at the beginning and end of the string.
-        text = text.trim { it <= ' ' }
+        intermediateText = intermediateText.trim()
 
-        return if (text.length > MAX_PREVIEW_LENGTH) {
-            text.substring(0, MAX_PREVIEW_LENGTH - 1) + "…"
+        return if (intermediateText.length > MAX_PREVIEW_LENGTH) {
+            intermediateText.substring(0, MAX_PREVIEW_LENGTH - 1) + "…"
         } else {
-            text
+            intermediateText
         }
     }
+
+    private fun normalizeLineBreaks(text: String) = text.replace(REGEX_CRLF, "\n")
+
+    private fun stripSignature(text: String): String {
+        return if (text.startsWith("-- \n")) {
+            ""
+        } else {
+            text.substringBefore("\n-- \n")
+        }
+    }
+
+    private fun extractUnquotedText(text: String): String {
+        val emailSections = EmailSectionExtractor.extract(text)
+        if (emailSections.isEmpty()) {
+            return ""
+        }
+
+        val firstEmailSection = emailSections.first()
+        val replySections = if (firstEmailSection.quoteDepth == 0) {
+            val replyEmailSections = emailSections.drop(1).filter { it.quoteDepth == 0 && it.isNotBlank() }
+            if (firstEmailSection.isQuoteHeaderOnly()) {
+                replyEmailSections
+            } else {
+                val firstSectionTextWithoutQuoteHeader = stripQuoteHeader(firstEmailSection)
+                listOf(firstSectionTextWithoutQuoteHeader) + replyEmailSections
+            }
+        } else {
+            emailSections.filter { it.quoteDepth == 0 && it.isNotBlank() }
+        }
+
+        return replySections.joinToString(separator = " […] ")
+    }
+
+    private fun stripQuoteHeader(emailSection: EmailSection): String {
+        val matchResult = REGEX_QUOTE_HEADER.find(emailSection) ?: return emailSection.toString()
+        return emailSection.substring(startIndex = 0, endIndex = matchResult.range.first)
+    }
+
+    private fun EmailSection.isQuoteHeaderOnly() = matches(REGEX_QUOTE_HEADER)
 
     companion object {
         private const val MAX_PREVIEW_LENGTH = 512
         private const val MAX_CHARACTERS_CHECKED_FOR_PREVIEW = 8192
+
+        private val REGEX_CRLF = "(\\r\\n|\\r)".toRegex()
+        private val REGEX_QUOTE_HEADER = "\\n*(?:[^\\n]+\\n?)+:\\n+$".toRegex()
     }
 }
